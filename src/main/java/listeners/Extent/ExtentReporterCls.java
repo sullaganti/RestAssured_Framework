bd4da1d9b82c5bbfb736a5498af864de3902432f
sullaganti <srinivasulla7007@gmail.com>
2025-06-10 23:48:39 +0530

    new AI helped GH actions reporting

--- src/main/java/listeners/Extent/ExtentReporterCls.java
+++ src/main/java/listeners/Extent/ExtentReporterCls.java
@@ -5,18 +5,25 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.ViewName;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ExtentReporterCls {
-    static ExtentReports extent;
-
-    public static ExtentReports ReportGenerator()
+    static ExtentReports extent;    public static ExtentReports ReportGenerator()
     {
         Date d = new Date();
         SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MMM-dd_HH_mm");
         String ReportFolderName = SDF.format(d);
-        String path = System.getProperty("user.dir")+"\\ExtentReports\\"+"ExtentReport"+ReportFolderName+".html";
+        
+        // Create ExtentReports directory if it doesn't exist
+        String reportDir = System.getProperty("user.dir") + "\\ExtentReports";
+        File dir = new File(reportDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        
+        String path = reportDir + "\\ExtentReport_" + ReportFolderName + ".html";
 
         ExtentSparkReporter ESR = new ExtentSparkReporter(path).viewConfigurer()
                 .viewOrder()
